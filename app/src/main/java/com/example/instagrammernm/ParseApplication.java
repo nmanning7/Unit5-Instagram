13517d023473ad7b7167ad12574e798215423daf
nmanning7 <nmanning117@rams.wssu.edu>
2021-03-22 23:54:03 -0400

    New Edits

--- app/src/main/java/com/example/instagrammernm/ParseApplication.java
+++ app/src/main/java/com/example/instagrammernm/ParseApplication.java
@@ -3,11 +3,13 @@ package com.example.instagrammernm;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
 
     public void onCreate() {
         super.onCreate();
+        ParseObject.registerSubclass(Post.class);
 
         Parse.initialize(new Parse.Configuration.Builder(this)
                 .applicationId("Lu9Gn0a8gObJBPZPLjx43APZnXhkOwFoz4wemRkd")
